21b2f52df35499c4a2d435b1ec0df218ac735ecf
roshankumar04 <90085148+roshankumar04@users.noreply.github.com>
2022-02-12 14:19:31 +0530

    Update main.js

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ menu_list_array = ["Veg Margherita Pizza",
     function add_item(){
         var htmldata;
 		var imgtags='<img id="im1" src="pizzaImg.png">'
-        var item=document.getElementById("").value;
+        var item=document.getElementById("add_item").value;
         // use the sort function as - 
         menu_list_array.sort();
         htmldata="";
